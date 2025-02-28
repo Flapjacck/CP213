73a50cba46f7905b764263106e2bea5c8cd18d07
Flapjacck <plkelly333@me.com>
2025-02-28 17:06:22 -0500

    Finished SingleLink.java

---
+++ Kell6733_a03/src/cp213/SingleList.java
@@ -0,0 +1,333 @@
+package cp213;
+
+/**
+ * A single linked list structure of <code>Node T</code> objects. These data
+ * objects must be Comparable - i.e. they must provide the compareTo method.
+ * Only the <code>T</code> object contained in the priority queue is visible
+ * through the standard priority queue methods. Extends the
+ * <code>SingleLink</code> class.
+ *
+ * @author David Brown
+ * @version 2025-01-05
+ * @param <T> this SingleList data type.
+ */
+public class SingleList<T extends Comparable<T>> extends SingleLink<T> {
+
+    /**
+     * Searches for the first occurrence of key in this SingleList. Private helper
+     * methods - used only by other ADT methods.
+     *
+     * @param key The object to look for.
+     * @return A pointer to the node previous to the node containing key.
+     */
+    private SingleNode<T> linearSearch(final T key) {
+
+	// your code here
+	SingleNode<T> current = this.front;
+	SingleNode<T> previous = null;
+	
+	while (current != null) {
+	    if (current.getEntity().compareTo(key) == 0) {
+		return previous;
+	    }
+	    previous = current;
+	    current = current.getNext();
+	}
+
+	return null;
+    }
+
+    /**
+     * Appends data to the end of this SingleList.
+     *
+     * @param entity The object to append.
+     */
+    public void append(final T entity) {
+
+	// your code here
+	SingleNode<T> node = new SingleNode<T>(entity, null);
+
+	if (this.front == null) {
+	    this.front = node;
+	    this.rear = this.front;
+
+	} else {
+	    this.rear.setNext(node);
+	    this.rear = this.rear.getNext();
+
+	}
+
+	this.length++;
+
+	return;
+    }
+
+    /**
+     * Removes duplicates from this SingleList. The list contains one and only one
+     * of each object formerly present in this SingleList. The first occurrence of
+     * each object is preserved.
+     */
+    public void clean() {
+
+	// your code here
+
+	return;
+    }
+
+    /**
+     * Combines contents of two lists into a third. Values are alternated from the
+     * origin lists into this SingleList. The origin lists are empty when finished.
+     * NOTE: data must not be moved, only nodes.
+     *
+     * @param left  The first list to combine with this SingleList.
+     * @param right The second list to combine with this SingleList.
+     */
+    public void combine(final SingleList<T> left, final SingleList<T> right) {
+
+	// your code here
+
+	return;
+    }
+
+    /**
+     * Determines if this SingleList contains key.
+     *
+     * @param key The key object to look for.
+     * @return true if key is in this SingleList, false otherwise.
+     */
+    public boolean contains(final T key) {
+
+	// your code here
+
+	return false;
+    }
+
+    /**
+     * Finds the number of times key appears in list.
+     *
+     * @param key The object to look for.
+     * @return The number of times key appears in this SingleList.
+     */
+    public int count(final T key) {
+
+	// your code here
+
+	return 0;
+    }
+
+    /**
+     * Finds and returns the object in list that matches key.
+     *
+     * @param key The object to search for.
+     * @return The object that matches key, null otherwise.
+     */
+    public T find(final T key) {
+
+	// your code here
+
+	return null;
+    }
+
+    /**
+     * Get the nth object in this SingleList.
+     *
+     * @param n The index of the object to return.
+     * @return The nth object in this SingleList.
+     * @throws ArrayIndexOutOfBoundsException if n is not a valid index.
+     */
+    public T get(final int n) throws ArrayIndexOutOfBoundsException {
+
+	// your code here
+
+	return null;
+    }
+
+    /**
+     * Determines whether two lists are identical.
+     *
+     * @param source The list to compare against this SingleList.
+     * @return true if this SingleList contains the same objects in the same order
+     *         as source, false otherwise.
+     */
+    public boolean equals(final SingleList<T> source) {
+
+	// your code here
+
+	return false;
+    }
+
+    /**
+     * Finds the first location of a object by key in this SingleList.
+     *
+     * @param key The object to search for.
+     * @return The index of key in this SingleList, -1 otherwise.
+     */
+    public int index(final T key) {
+
+	// your code here
+
+	return 0;
+    }
+
+    /**
+     * Inserts object into this SingleList at index i. If i greater than the length
+     * of this SingleList, append data to the end of this SingleList.
+     *
+     * @param i      The index to insert the new data at.
+     * @param entity The new object to insert into this SingleList.
+     */
+    public void insert(int i, final T entity) {
+
+	// your code here
+
+	return;
+    }
+
+    /**
+     * Creates an intersection of two other SingleLists into this SingleList. Copies
+     * data to this SingleList. left and right SingleLists are unchanged. Values
+     * from left are copied in order first, then objects from right are copied in
+     * order.
+     *
+     * @param left  The first SingleList to create an intersection from.
+     * @param right The second SingleList to create an intersection from.
+     */
+    public void intersection(final SingleList<T> left, final SingleList<T> right) {
+
+	// your code here
+
+	return;
+    }
+
+    /**
+     * Finds the maximum object in this SingleList.
+     *
+     * @return The maximum object.
+     */
+    public T max() {
+
+	// your code here
+
+	return null;
+    }
+
+    /**
+     * Finds the minimum object in this SingleList.
+     *
+     * @return The minimum object.
+     */
+    public T min() {
+
+	// your code here
+
+	return null;
+    }
+
+    /**
+     * Inserts object into the front of this SingleList.
+     *
+     * @param entity The object to insert into the front of this SingleList.
+     */
+    public void prepend(final T entity) {
+
+	// your code here
+
+	return;
+    }
+
+    /**
+     * Finds, removes, and returns the object in this SingleList that matches key.
+     *
+     * @param key The object to search for.
+     * @return The object matching key, null otherwise.
+     */
+    public T remove(final T key) {
+
+	// your code here
+
+	return null;
+    }
+
+    /**
+     * Removes the object at the front of this SingleList.
+     *
+     * @return The object at the front of this SingleList.
+     */
+    public T removeFront() {
+
+	// your code here
+
+	return null;
+    }
+
+    /**
+     * Finds and removes all objects in this SingleList that match key.
+     *
+     * @param key The object to search for.
+     */
+    public void removeMany(final T key) {
+
+	// your code here
+
+	return;
+    }
+
+    /**
+     * Reverses the order of the objects in this SingleList.
+     */
+    public void reverse() {
+
+	// your code here
+
+	return;
+    }
+
+    /**
+     * Splits the contents of this SingleList into the left and right SingleLists.
+     * Moves nodes only - does not move object or call the high-level methods insert
+     * or remove. this SingleList is empty when done. The first half of this
+     * SingleList is moved to left, and the last half of this SingleList is moved to
+     * right. If the resulting lengths are not the same, left should have one more
+     * object than right. Order is preserved.
+     *
+     * @param left  The first SingleList to move nodes to.
+     * @param right The second SingleList to move nodes to.
+     */
+    public void split(final SingleList<T> left, final SingleList<T> right) {
+
+	// your code here
+
+	return;
+    }
+
+    /**
+     * Splits the contents of this SingleList into the left and right SingleLists.
+     * Moves nodes only - does not move object or call the high-level methods insert
+     * or remove. this SingleList is empty when done. Nodes are moved alternately
+     * from this SingleList to left and right. Order is preserved.
+     *
+     * @param left  The first SingleList to move nodes to.
+     * @param right The second SingleList to move nodes to.
+     */
+    public void splitAlternate(final SingleList<T> left, final SingleList<T> right) {
+
+	// your code here
+
+	return;
+    }
+
+    /**
+     * Creates a union of two other SingleLists into this SingleList. Copies object
+     * to this list. left and right SingleLists are unchanged. Values from left are
+     * copied in order first, then objects from right are copied in order.
+     *
+     * @param left  The first SingleList to create a union from.
+     * @param right The second SingleList to create a union from.
+     */
+    public void union(final SingleList<T> left, final SingleList<T> right) {
+
+	// your code here
+
+	return;
+    }
+}
