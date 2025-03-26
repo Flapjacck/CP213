edca4e8e216a85554e1d504dbe2bf040be96b1ee
Flapjacck <plkelly333@me.com>
2025-03-26 19:44:00 -0400

    Finished menu files

---
+++ Kell6733_a05/src/cp213/Menu.java
@@ -0,0 +1,115 @@
+package cp213;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Stores a List of MenuItems and provides a method return these items in a
+ * formatted String. May be constructed from an existing List or from a file
+ * with lines in the format:
+ *
+ * <pre>
+1.25 hot dog
+10.00 pizza
+...
+ * </pre>
+ *
+ * @author your name here
+ * @author Abdul-Rahman Mawlood-Yunis
+ * @author David Brown
+ * @version 2025-01-05
+ */
+public class Menu {
+
+    // Attributes.
+
+    // define a List of MenuItem objects
+    // Note that this must be a *List* of some flavour
+    // @See
+    // https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/util/List.html
+
+    // your code here
+    private final List<MenuItem> items = new ArrayList<MenuItem>(); // Holds all menu items
+
+    /**
+     * Creates a new Menu from an existing List of MenuItems. MenuItems are copied
+     * into the Menu List.
+     *
+     * @param items an existing List of MenuItems.
+     */
+    public Menu(List<MenuItem> items) {
+
+	// your code here
+	this.items.addAll(items); // Copy items into internal list
+    }
+
+    /**
+     * Constructor from a Scanner of MenuItem strings. Each line in the Scanner
+     * corresponds to a MenuItem. You have to read the Scanner line by line and add
+     * each MenuItem to the List of items.
+     *
+     * @param fileScanner A Scanner accessing MenuItem String data.
+     */
+    public Menu(Scanner fileScanner) {
+
+	// your code here
+	while (fileScanner.hasNextLine()) {
+            String line = fileScanner.nextLine().trim();
+            if (!line.isEmpty()) {
+                Scanner lineScanner = new Scanner(line);
+                double price = lineScanner.nextDouble();      // Get price
+                String name = lineScanner.nextLine().trim();  // Get name
+                this.items.add(new MenuItem(name, price));    // Add to list
+                lineScanner.close();
+            }
+        }
+
+    }
+
+    /**
+     * Returns the List's i-th MenuItem.
+     *
+     * @param i Index of a MenuItem.
+     * @return the MenuItem at index i
+     */
+    public MenuItem getItem(int i) {
+
+	// your code here
+	return items.get(i);
+    }
+
+    /**
+     * Returns the number of MenuItems in the items List.
+     *
+     * @return Size of the items List.
+     */
+    public int size() {
+
+	// your code here
+
+	return items.size();
+    }
+
+    /**
+     * Returns the Menu items as a String in the format:
+     *
+     * <pre>
+    5) poutine      $ 3.75
+    6) pizza        $10.00
+     * </pre>
+     *
+     * where n) is the index + 1 of the MenuItems in the List.
+     */
+    @Override
+    public String toString() {
+
+	// your code here
+	StringBuilder result = new StringBuilder();
+	for (int index = 0; index < items.size(); index++) {
+	    result.append(" ").append(index + 1).append(") ").append(items.get(index).toString()).append("\n");
+	}
+
+	return result.toString();
+    }
+}
